16fd3a5e4c364dbbf2239a0258d5a2e62ecce4ba
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2022-10-01 21:03:43 -0300

    Working in changing theme part

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let operation = ""
 const cuadro = document.getElementById('ope')
 const key = document.querySelectorAll("#num")
-let opInt = []
 const eq = document.getElementById('equal')
 const reset = document.getElementById('reset')
 const del = document.getElementById('del')
+const fonts = [document.getElementById('selector') , document.getElementById('thnumb'), document.getElementById('ope')]
+const bgpad  = [document.getElementById('keys'), document.getElementById('chooseTheme')]
+const bgscrn = document.getElementById('screen')
+
 reset.addEventListener('click',rst)
 eq.addEventListener('click',equal)
 del.addEventListener('click',dlt)
@@ -31,5 +34,44 @@ function dlt(){
     operation  =  operation2
     operation2 = ""
     cuadro.innerHTML = operation
+}
+
+let range = document.getElementById('chooseTheme')
+function change(){ 
+    if(range.value==0){
+        document.body.style.backgroundColor = "hsl(222, 26%, 31%)";
+        bgscrn.style.backgroundColor = 'hsl(224, 36%, 15%)'
+     
+        fonts.forEach((a,index  )=> {
+            fonts[index].style.color = 'white'
+        })
+        bgpad.forEach((a,index  )=> {
+            bgpad[index].style.backgroundColor = 'hsl(223, 31%, 20%)'
+        })
+
+    }
+    
+if(range.value==1){
+    document.body.style.backgroundColor = "hsl(0, 0%, 90%)";
+    bgscrn.style.backgroundColor = 'hsl(0, 0%, 93%)'
+
+    fonts.forEach((a,index  )=> {
+        fonts[index].style.color = 'hsl(60, 10%, 19%)'
+    })
+    bgpad.forEach((a,index  )=> {
+        bgpad[index].style.backgroundColor = 'hsl(0, 5%, 81%)'
+    })
+}
+if(range.value==2){
+    document.body.style.backgroundColor = "hsl(268, 75%, 9%)";
+    bgscrn.style.backgroundColor = 'hsl(268, 71%, 12%)'
+
+    fonts.forEach((a,index  )=> {
+        fonts[index].style.color = 'hsl(52, 100%, 62%)'
+    })
+    bgpad.forEach((a,index  )=> {
+        bgpad[index].style.backgroundColor = 'hsl(268, 71%, 12%)'
+    })
+}
 }
 
